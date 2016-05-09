42cfc1910c3965bca3f7fe30232df070e8e123bf
kantharuban <k.kantharuban@sg.tribalworldwide.com>
2016-05-09 14:53:39 +0800

    models

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+
+
+class SignupForm(Form):
+
+    first_name = StringField('First name')
+    last_name = StringField('Last name')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    submit = SubmitField('sign up')
+
--- routes.py
+++ routes.py
@@ -1,6 +1,14 @@
 from flask import  Flask, render_template
+from models import db
+from forms import SignupForm
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/find_a_place'
+db.init_app(app)
+
+app.secret_key = "development key"
 
 @app.route("/")
 def index():
@@ -10,6 +18,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template('signup.html', form=form)
 
 
 if __name__ == "__main__":
