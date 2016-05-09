52f93b88d7c7e6562c603a307ec0121ab09445be
kantharuban <k.kantharuban@sg.tribalworldwide.com>
2016-05-09 16:06:09 +0800

    signup page

--- forms.py
+++ forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 
 class SignupForm(Form):
 
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First name', validators=[DataRequired("Please enter your first name")])
+    last_name = StringField('Last name',  validators=[DataRequired("Please enter your last name")])
+    email = StringField('Email',  validators=[DataRequired("Please enter your Email")])
+    password = PasswordField('Password',  validators=[DataRequired("Please enter your password")])
     submit = SubmitField('sign up')
 
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template,request
 from models import db
 from forms import SignupForm
 
@@ -10,6 +10,7 @@ db.init_app(app)
 
 app.secret_key = "development key"
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -19,10 +20,18 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/signup")
+
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
-    return render_template('signup.html', form=form)
+
+    if request.method == 'POST':
+        if form.validate()== False:
+            return render_template('signup.html', form=form)
+        else:
+            return 'Success!'
+    elif request.method == "GET":
+        return render_template('signup.html', form=form)
 
 
 if __name__ == "__main__":
