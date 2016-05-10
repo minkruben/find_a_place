b926ca2217bcf07d8bc759fe30585f47f627a65a
kantharuban <k.kantharuban@sg.tribalworldwide.com>
2016-05-10 11:59:54 +0800

    signin

--- forms.py
+++ forms.py
@@ -1,14 +1,19 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(Form):
 
     first_name = StringField('First name', validators=[DataRequired("Please enter your first name")])
     last_name = StringField('Last name',  validators=[DataRequired("Please enter your last name")])
-    email = StringField('Email',  validators=[DataRequired("Please enter your Email")])
-    password = PasswordField('Password',  validators=[DataRequired("Please enter your password")])
+    email = StringField('Email',  validators=[DataRequired("Please enter your Email"), Email("Please enter a valid email")])
+    password = PasswordField('Password',  validators=[DataRequired("Please enter your password"), Length(min=6, message="Passwords must be 6 character")])
     submit = SubmitField('sign up')
 
+
+class SigninForm(Form):
+
+    email = StringField('Email', validators=[DataRequired('Please Enter your Email address'), Email("Please enter a valid email")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password")])
+    submit = SubmitField("Sign in")
--- routes.py
+++ routes.py
@@ -1,11 +1,11 @@
-from flask import  Flask, render_template,request
-from models import db
-from forms import SignupForm
+from flask import  Flask, render_template, request, session, redirect, url_for
+from models import db, User
+from forms import SignupForm, SigninForm
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/find_a_place'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:Gilchrist1~@localhost:5432/find_a_place'
 db.init_app(app)
 
 app.secret_key = "development key"
@@ -29,9 +29,43 @@ def signup():
         if form.validate()== False:
             return render_template('signup.html', form=form)
         else:
-            return 'Success!'
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
+           # return 'Success!'
     elif request.method == "GET":
         return render_template('signup.html', form=form)
+
+
+@app.route("/signin", methods=['GET', 'POST'])
+def signin():
+    form = SigninForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template('signin.html', form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.dara
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('signin'))
+
+    elif request.method == 'GET':
+        return render_template('signin.html', form=form)
+
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
